f193618e08e75e029e3fc735dd6c6b7d7a016825
Sean Misra <ssm937@g.harvard.edu>
2017-05-25 09:37:32 -0400

    Clean-up

--- shortest_encapsulator.py
+++ shortest_encapsulator.py
@@ -2,33 +2,35 @@ import sys
 
 
 if __name__ == '__main__':
-    all_strings = []
 
+    # exit if no strings provided
     if len(sys.argv) == 1:
         print("No strings entered. You can enter them, space-separated, via the command-line.")
-        exit
-    else:
-        # skip for argument
-        iter_args = iter(sys.argv)
-        next(iter_args)
+        sys.exit()
 
-        # add all other arguments (strings) to a string list
-        for word in iter_args:
-            all_strings.append(word)
+    all_strings = []
 
-        # loop through each element in string list (except the last)
-        for elem in range (0, len(all_strings) - 1):
-            string_length = len(all_strings[elem])
+    # skip first argument
+    iter_args = iter(sys.argv)
+    next(iter_args)
 
-            # check chars in current elem against chars in next elem
-            # if duplicates are found, delete them
-            for i in range(1, string_length):
-                if all_strings[elem][-i:] == all_strings[elem + 1][:i]:
-                    all_strings[elem] = all_strings[elem][:-i]
+    # add all other arguments (strings) to a string list
+    for word in iter_args:
+        all_strings.append(word)
 
-        # combine strings into a final string - output it
-        final_string = ""
-        for word in all_strings:
-            final_string += word
+    # loop through each element in string list (except the last)
+    for elem in range (0, len(all_strings) - 1):
+        string_length = len(all_strings[elem])
 
-        print(final_string)
+        # check chars in current elem against chars in next elem
+        # if duplicates are found, delete them
+        for i in range(1, string_length):
+            if all_strings[elem][-i:] == all_strings[elem + 1][:i]:
+                all_strings[elem] = all_strings[elem][:-i]
+
+    # combine strings into a final string - output it
+    final_string = ""
+    for word in all_strings:
+        final_string += word
+
+    print(final_string)
